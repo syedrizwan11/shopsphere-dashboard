53d9802391dccc2fd1fafa1d58a2b886816c888e
syedrizwan11 <rsg.sr27@gmail.com>
2025-08-30 19:29:15 +0500

    app icon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next"
 import { Geist, Geist_Mono } from "next/font/google"
 import "./globals.css"
-import { SideBar } from "@/components/layouts/sidebar/Sidebar"
+import { SideBar } from "@/components/layouts/sidebar"
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
